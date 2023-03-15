2dded52edc4aa815d22562f3d08124fbb1f59802
FantasticalPotatoguin <38575794+FantasticalPotatoguin@users.noreply.github.com>
2023-03-15 14:54:27 -0400

    working on stuff
    background.js can communicate to popup.js via url parameters
    Working on functionality for images

---
+++ background.js
@@ -0,0 +1,36 @@
+
+
+chrome.runtime.onInstalled.addListener( () => {
+chrome.contextMenus.create({
+    "id": "organize",
+    "title": "Save",
+    "contexts": ["selection", "link", "image"]
+})
+});
+
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if(clickData.menuItemId == "organize"){
+        var urlParams = "";
+        if (clickData.mediaType == "image"){
+            urlParams = "?image=true";
+        }
+        else
+        {
+            urlParams = "";
+        }
+        
+        //if (clickData.mediaType == "image"){
+            chrome.windows.create(
+                {type: "popup",
+                url: "popup.html" + urlParams,//clickData.linkUrl,//
+                height: 300,
+                width: 200, 
+                focused: true
+                //Still need to figure out how to make the popup appear at the cursor location
+                }
+            )
+//}
+}
+}
+);
--- popup.js
+++ popup.js
@@ -1,13 +1,32 @@
-/*const tabs = await chrome.tabs.query({
-url: [
 
-]
-
-})*/
-
+function isImgUrl(url) {
+    // function obtained from https://bobbyhadz.com/blog/javascript-check-if-url-is-image
+    //extra image filetypes from https://www.techonthenet.com/html/elements/img_tag.php
+   return /\.(jpg|jpeg|png|webp|avif|gif|svg|bmp|apng)$/.test(url);
+    }
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
 {
-    var tab = tabs[0];
-    alert(tab.url);
+    var testUrl = "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png"
+if (isImgUrl(testUrl)){
+    const queryString = window.location.search;
+document.getElementById("link").innerHTML = queryString//testUrl
+
+}
+else{
+    document.getElementById("link").innerHTML = "error";
+}
 })
+
+
+
+
+
+
+/* if (has parameters: image)
+    image stuff
+
+if (has parameters: text) 
+    text stuff
+if (has no parameters)
+    Page */
