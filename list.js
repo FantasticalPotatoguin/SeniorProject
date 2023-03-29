b80bf3167498ba3cfed0f9c9fff3ed3ed249fd9b
FantasticalPotatoguin <38575794+FantasticalPotatoguin@users.noreply.github.com>
2023-03-29 16:29:07 -0400

    Basic functionality complete
    It's not pretty but it works.
    
    Still needs done:
    
    Support for dates
    Display images in library page
    UI overhaul
    Settings page
    Additional lists

--- list.js
+++ list.js
@@ -8,15 +8,26 @@ var store = tx.objectStore("List1");
 
 
 
-var req = store.get(1);
+
+let req = store.openCursor();
+let allItems = [];
+
 
 req.onsuccess = function(event) {
-    let note = event.target.result;
-    if (note) {
-        document.getElementById("nickname").innerHTML = note.nickname;
-        document.getElementById("rating").innerHTML = "Rating:" + note.rank;
-        document.getElementById("dateAdded").innerHTML = "Date added:" + note.dateAdded;
-        document.getElementById("content").innerHTML = note.content;
+    //loop is not necessary because IndexedDB fires the "onsuccess" event 
+    //every time the cursor is successfully advanced
+    let cursor = event.target.result;
+    var htmlToAdd = "";
+
+    if (cursor) {
+        htmlToAdd += "<h1 class = 'nickname'>" + cursor.value.nickname + "</h1>";
+        htmlToAdd += "<p class = 'rating'> Rating:" + cursor.value.rank + "</p>";
+        htmlToAdd += "<div class = 'content'>" + cursor.value.content + "</div>";
+        htmlToAdd += "<p class = 'date'> Date:" + cursor.value.dateAdded + "</p>";
+
+        document.getElementById("page").innerHTML += htmlToAdd;
+        htmlToAdd = "";
+        cursor.continue();
     }
     else {
         document.getElementById("title").innerHTML = "error";
